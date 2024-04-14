a4566d63a05f22ddf29d378ad176cad1a771bb3b
Tommy Johannesson <atjohannesson@gmail.com>
2024-04-14 17:49:40 +0200

    First commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+    #[Route("/lucky", name: "lucky")]
+    public function lucky(): Response
+    {
+        $number = random_int(0, 100);
+        $bodycolornumber = random_int(0, 6);
+        $backgroundnumber = random_int(0,6);
+        $fontcolornumber = random_int(0,6);
+        $colorwheel = ["red", "green", "blue", "yellow", "black", "grey", "white"];
+        $colorwheelname = ["röd", "grön", "blå", "gul", "svart", "grå", "vit"];
+
+        $data = [
+            'luckynumber' => $number,
+            'background' => $colorwheel[$backgroundnumber],
+            'backgroundname' => $colorwheelname[$backgroundnumber],
+            'fontcolor' => $colorwheel[$fontcolornumber],
+            'fontcolorname' => $colorwheelname[$fontcolornumber],
+            'bodycolor' => $colorwheel[$bodycolornumber],
+            'bodycolorname' => $colorwheelname[$bodycolornumber]
+        ];
+
+        return $this->render('lucky.html.twig', $data);
+    }
+
+    #[Route("/api/", name:"api_index")]
+    public function api_index(): Response
+    {
+        $data = [
+            'lucky-message' => 'Hi there!'
+        ];
+
+        $response = new Response();
+        $response->setContent(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $this->render('api.html.twig', $data);
+    }
+}
---
+++ src/Controller/MainControllerJson.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainControllerJson
+{
+    #[Route("/api/quote", name:"jsonquote")]
+    public function jsonQuote(): Response
+    {
+        date_default_timezone_set('Europe/Stockholm');
+        $randomquote = random_int(0, 6); 
+        $quotes = [
+            'Nära skjuter ingen hare här på fortet - Gunde Svan',
+            'Bazinga! - Sheldon Cooper',
+            'Vart är vi på väg? - Ingvar Oldsberg, Kristian Luuk',
+            'Du har vunnit hästen! - Leif "Loket" Olsson',
+            'a² + b² = c² - Pythagoras',
+            'My Precious - Gollum',
+            'Where we´re going, we don´t need roads. - Dr. Emmet Brown',
+        ];
+        $date = new DateTime();
+
+        $data = [
+            'today' => $date->format('Y-m-d'),
+            'todays_quote' => $quotes[$randomquote],
+            'timestamp' => $date->format('Y-m-d H:i:s'),
+        ];
+
+        $response = new Response();
+        $response->setContent(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
