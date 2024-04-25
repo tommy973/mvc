79c4b24e2c669549fe852f1fa06ce8e5264555d4
Tommy Johannesson <atjohannesson@gmail.com>
2024-04-25 21:31:58 +0200

    Made changes to how the suit and rank are printed out

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -22,4 +22,24 @@ class Card {
     {
         return $this->rank;
     }
+
+    public function getSuitAsString() : ?string
+    {
+        $suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
+
+        return $suits[$this->suit];
+    }
+
+    public function getRankAsString() : ?string
+    {
+        $ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+
+        return $ranks[$this->rank];
+    }
+
+    public function getCardAsString() : ?string
+    {
+        $currentcard = $this->getRankAsString() . " of " . $this->getSuitAsString();
+        return $currentcard;
+    }
 }
