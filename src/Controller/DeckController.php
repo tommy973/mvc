96e59004d5eb8d1c8c585d96c8252548b6930abb
Tommy Johannesson <atjohannesson@gmail.com>
2024-04-25 21:34:59 +0200

    Added route to deal cards to multiple players

--- src/Controller/DeckController.php
+++ src/Controller/DeckController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Tommy\Card\Card;
+use Tommy\Card\CardHand;
+use Tommy\Card\DeckOfCards;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -10,80 +12,39 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Tommy\Card\DeckOfCards;
 
 class DeckController extends AbstractController
 {
+    // Main landing route
     #[Route("/card", name: "card_landing")]
     public function card_landing(
         SessionInterface $session
     ): Response
     {
-        // $testCard = new Card();
-
-        // $testCard->draw();
-
-        // $data = [
-        //     'suit' => $testCard->getSuit(),
-        //     'rank' => $testCard->getRank(),
-        // ];
-
-        // $testDeck = new DeckOfCards();
-
-        // $shuffledDeck = $testDeck->shuffleDeck();
-
-        // $sortedDeck = $testDeck->sortDeck();
-
-        // $testDeck->shuffleDeck();
-
-        // $testDeck->drawSingleCard();
-
-        // $data = [
-        //     'deckofcards' => $testDeck->getCardsAsString(),
-        //     'shuffledDeck' => $testDeck->shuffleDeck(),
-        //     'shuffledcards' => $testDeck->getCardsAsString(),
-        //     'sortedDeck' => $testDeck->sortDeck(),
-        //     'sortedcards' => $testDeck->getCardsAsString(),
-        // ];
-
-        // $session->set("cardsuit", $data['suit']);
-        // $session->set("cardrank", $data['rank']);
-
         return $this->render('card.html.twig');
     }
 
+    // Sorts deck
     #[Route("/card/deck", name: "all_cards_in_deck")]
     public function allCardsInDeck(
         SessionInterface $session
     ): Response
     {
-        // $testCard = new Card();
-
-        // $testCard->draw();
-
-        // $data = [
-        //     'suit' => $testCard->getSuit(),
-        //     'rank' => $testCard->getRank(),
-        // ];
+        // Checks if there is a deck in session. If not, a deck is created.
         $deck = $session->get("currentdeck");
-        if (empty($currentDeck)) {
+
+        if (empty($deck)) {
             $deck = new DeckOfCards();
         } else {
             $deck->sortDeck();
         }
 
-        // $testDeck->shuffleDeck();
-
-        // $testDeck->drawSingleCard();
-
         $data = [
-            'deckofcards' => $deck->getCardsAsString(),
+            'deckofcards' => $deck->getDeckAsStringArray(),
+            'deck' => $deck,
         ];
 
         $session->set("currentdeck", $deck);
-
-        // $session->set("cardsuit", $data['suit']);
-        // $session->set("cardrank", $data['rank']);
 
         return $this->render('cards/deck.html.twig', $data);
     }
@@ -93,29 +54,18 @@ class DeckController extends AbstractController
         SessionInterface $session
     ): Response
     {
-        // $testCard = new Card();
-
-        // $testCard->draw();
-
-        // $data = [
-        //     'suit' => $testCard->getSuit(),
-        //     'rank' => $testCard->getRank(),
-        // ];
-
         $deck = new DeckOfCards();
 
         $deck->shuffleDeck();
 
-        // $testDeck->drawSingleCard();
-
         $data = [
-            'deckofcards' => $deck->getCardsAsString(),
+            'deckofcards' => $deck->getDeckAsStringArray(),
+            'deck' => $deck,
         ];
 
         $session->set("currentdeck", $deck);
-        // $session->set("cardrank", $data['rank']);
 
-        return $this->render('cards/deck.html.twig', $data);
+        return $this->render('cards/shuffle.html.twig', $data);
     }
 
     #[Route("/card/deck/draw", name: "draw_card")]
@@ -126,20 +76,23 @@ class DeckController extends AbstractController
 
         $deck = $session->get("currentdeck");
 
+        if (empty($deck)) {
+            $deck = new DeckOfCards();
+        }
+
         $cardlimit = $deck->numberOfCardsInDeck();
 
         if ($cardlimit == 0) {
             throw new \Exception("Leken är slut, du kan inte dra fler kort.");
         }
         
+        $singleCard = $deck->drawSingleCard();
+
         $drawnCard = [];
-        $drawnCard[] = $deck->drawSingleCard();
-
-
-        // $testDeck->drawSingleCard();
+        $drawnCard[] = $singleCard->getCardAsString();
 
         $data = [
-            'deckofcards' => $deck->getCardsAsString(),
+            'deckofcards' => $deck->getDeckAsStringArray(),
             'drawncard' => $drawnCard,
             'numberofcards' => $deck->numberOfCardsInDeck()
         ];
@@ -157,26 +110,98 @@ class DeckController extends AbstractController
     {
         $deck = $session->get("currentdeck");
 
+        if (empty($deck)) {
+            $deck = new DeckOfCards();
+        }
+
         $cardlimit = $deck->numberOfCardsInDeck();
 
         if ($number > $cardlimit) {
             throw new \Exception("Du har dragit fler kort än som finns i leken");
         }
 
-        $drawncards = [];
-        
         for ($i = 0; $i < $number; $i++) {
-            $drawncards[] = $deck->drawSingleCard();
+            $singleCard = $deck->drawSingleCard();
+            $drawnCard[] = $singleCard->getCardAsString();
         }
 
         $data = [
-            'deckofcards' => $deck->getCardsAsString(),
-            'drawncard' => $drawncards,
+            'deckofcards' => $deck->getDeckAsStringArray(),
+            'drawncard' => $drawnCard,
             'numberofcards' => $deck->numberOfCardsInDeck()
         ];
 
         $session->set("currentdeck", $deck);
 
         return $this->render('cards/drawcard.html.twig', $data);
+    }
+
+    #[Route("/card/deck/dealinit", name: "deal_cards_init", methods: ['GET'])]
+    public function DealCardsInit(
+        SessionInterface $session
+    ): Response
+    {
+        return $this->render('cards/dealcard.html.twig');
+    }
+
+    #[Route("/card/deck/dealinit", name: "deal_cards_init_post", methods: ['POST'])]
+    public function DealCardsInitPost(
+        Request $request,
+        SessionInterface $session
+    ): Response
+    {
+        $numPlayers = $request->request->get('num_players');
+        $numCards = $request->request->get('num_cards');
+
+        return $this->redirectToRoute('deal_cards', ['players' => $numPlayers, 'cards' => $numCards]);
+    }
+
+
+    #[Route("/card/deck/deal/{players<\d+>}/{cards<\d+>}", name: "deal_cards")]
+    public function DealCardsFromDeck(
+        SessionInterface $session,
+        int $players,
+        int $cards
+    ): Response
+    {
+        $deck = $session->get("currentdeck");
+
+        if (empty($deck)) {
+            $deck = new DeckOfCards();
+        }
+
+        $cardlimit = $deck->numberOfCardsInDeck();
+
+        if ($players < 1 || $players > 52) {
+            throw new \Exception("Du måste välja ett giltigt antal spelare (1 - 52)");
+        }
+
+        if (($cards * $players) > $cardlimit){
+            throw new \Exception("Antalet kort räcker inte till alla spelare");
+        }
+
+        for ($i = 0; $i < $players; $i++) {
+            $cardHands[] = new CardHand();
+            for ($j = 0; $j < $cards; $j++) {
+                $cardHands[$i]->add($deck->drawSingleCard());
+            }
+            // $temp['cardhand' . $i] = $cardHands[$i];
+            $cardhandsArray[] = $cardHands[$i]->getHandAsString();
+            $session->set("cardhand$i", $cardHands[$i]);
+        }
+        $session->set("currentdeck", $deck);
+
+        $deck->sortDeck();
+
+        $data = [
+            'numberofplayers' => $players,
+            'numberofcards' => $cards,
+            'cardhand' => $cardhandsArray,
+            'deckofcards' => $deck->getDeckAsStringArray(),
+        ];
+
+        
+
+        return $this->render('cards/dealtcard.html.twig', $data);
     }
 }
