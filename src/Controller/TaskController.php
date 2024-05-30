44d2803f32d087e88ea570577af6c9a0927c4083
Tommy Johannesson <atjohannesson@gmail.com>
2024-05-30 09:43:03 +0200

    First commit

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+
+// src/Controller/TaskController.php
+namespace App\Controller;
+
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Form\Type\TaskType;
+
+class TaskController extends AbstractController
+{
+    public function new(Request $request): Response
+    {
+        // creates a task object and initializes some data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTimeImmutable('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        // ...
+    }
+}
