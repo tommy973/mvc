d92c7f525c3e730df8a7483dd7d94b1f34b52d22
Tommy Johannesson <atjohannesson@gmail.com>
2026-02-22 18:08:54 +0100

    Fixed Coding standard with php-CS-fixer

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -23,21 +23,21 @@ class Card
         return $this->rank;
     }
 
-    public function getSuitAsString() : ?string
+    public function getSuitAsString(): ?string
     {
         $suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
 
         return $suits[$this->suit];
     }
 
-    public function getRankAsString() : ?string
+    public function getRankAsString(): ?string
     {
         $ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
 
         return $ranks[$this->rank];
     }
 
-    public function getCardAsString() : ?string
+    public function getCardAsString(): ?string
     {
         $currentcard = $this->getRankAsString() . " of " . $this->getSuitAsString();
         return $currentcard;
