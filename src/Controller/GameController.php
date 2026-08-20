f8c1723be67701da5718bd72e6f9ff69d77797c5
Tommy Johannesson <atjohannesson@gmail.com>
2026-08-20 17:15:09 +0200

    First commit

---
+++ src/Controller/GameController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Controller;
+
+use App\Card\Card;
+use App\Card\CardHand;
+use App\Card\DeckOfCards;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    // Main landing route
+    #[Route("/game", name: "game_landing")]
+    public function gameLanding(
+        SessionInterface $session
+    ): Response {
+        return $this->render('game.html.twig');
+    }
+
+    // Documentation
+    #[Route("/game/doc", name: "game_doc")]
+    public function gameDoc(
+        SessionInterface $session
+    ): Response {
+        return $this->render('game/doc.html.twig');
+    }
+
+    // Game Start Route
+    #[Route("/game/gamestart", name: "game_start")]
+    public function gameStart(
+        SessionInterface $session
+    ): Response {
+        $game_started = $session->get("game_started");
+
+        if (empty($game_started)) {
+            $game_started = false;
+        }
+
+        $data = [
+            'gamestarted' => $game_started
+        ];
+
+        return $this->render('game/gamestart.html.twig', $data);
+    }
+
+    // Game Init Route
+    #[Route("game/gameinit", name: "game_init")]
+    public function gameInit(
+        SessionInterface $session
+    ): Response {
+        $gamedeck = new DeckOfCards();
+        $playerHand = new CardHand();
+        $bankHand = new CardHand();
+
+        $session->set("gamedeck", $gamedeck);
+        $session->set("playerhand", $playerHand);
+        $session->set("bankhand", $bankHand);
+        $session->set("gamephase", "playersturn");
+
+        return $this->redirectToRoute('game_play');
+    }
+
+    // Play Game Route
+    #[Route("game/gameplay", name: "game_play")]
+    public function gamePlay(
+        SessionInterface $session
+    ): Response {
+        $gamephase = $session->get("gamephase");
+        $outcome = "";
+        $loss = false;
+
+        $playerHand = $session->get("playerhand");
+        $playerHandString = $playerHand->getHandAsString();
+        $playerHandSum = $playerHand->getHandSum();
+        if ($playerHandSum > 21) {
+            $loss = true;
+            $outcome = "Du fick över 21. Banken vinner.";
+        }
+
+        $bankHand = $session->get("bankhand");
+        $bankHandString = $bankHand->getHandAsString();
+        $bankHandSum = $bankHand->getHandSum();
+        if ($bankHandSum > 21) {
+            $loss = true;
+            $outcome = "Banken fick över 21. Du vann vinner.";
+        }
+
+        // Jämför om det är decide
+        if ($gamephase == "decide") {
+            if ($playerHandSum > $bankHandSum) {
+                $outcome = "Du";
+            } else {
+                $outcome = "Banken";
+            }
+        }
+
+        $data = [
+            'playerdrawncards' => $playerHandString,
+            'playersum' => $playerHandSum,
+            'bankdrawncards' => $bankHandString,
+            'banksum' => $bankHandSum,
+            'gamephase' => $gamephase,
+            'outcome' => $outcome,
+            'loss' => $loss,
+            'gamedeck' => $session->get("gamedeck"),
+            'playerhand' => $session->get("playerhand"),
+            'bankhand' => $session->get("bankhand"),
+            'gamestarted' => $session->get("gamestarted")
+        ];
+        
+        return $this->render('game/gameplay.html.twig', $data);
+    }
+
+    // Game Draw a card Route
+    #[Route("game/gamedrawcard", name: "game_drawcard")]
+    public function gameDrawCard(
+        SessionInterface $session
+    ): Response {
+        $gamephase = $session->get("gamephase");
+        // Draw a card
+        $gameDeck = $session->get("gamedeck");
+        $drawnCard = $gameDeck->drawSingleCard();
+
+        // Lägg kort i spelarens hand om det är spelarens tur
+        if ($gamephase == "playersturn") {
+            $playerHand = $session->get("playerhand");
+            $playerHand->add($drawnCard);
+            $session->set("playerhand", $playerHand);
+        }
+
+        // Lägg kort i bankens hand om det är bankens tur
+        if ($gamephase == "banksturn") {
+            $bankHand = $session->get("bankhand");
+            $bankHand->add($drawnCard);
+            $session->set("bankhand", $bankHand);
+        }
+
+
+        
+        // $playerHand = $session->get("playerhand");
+        // $bankHand = $session->get("bankhand");
+
+
+        // $playerHand->add($drawnCard);
+
+        $session->set("gamedeck", $gameDeck);
+        // $session->set("playerhand", $playerHand);
+        // $session->set("bankhand", $bankHand);
+        // $session->set("gamestarted", true);
+
+        return $this->redirectToRoute('game_play');
+    }
+
+    // Game Pass Route
+    #[Route("game/gamepass", name: "game_pass")]
+    public function gamePass(
+        SessionInterface $session
+    ): Response {
+        $gamephase = $session->get("gamephase");
+        
+        // Om spelaren är nöjd så ändras det till bankens tur
+        if ($gamephase == "playersturn") {
+            $session->set("gamephase", "banksturn");
+        }
+
+        // Om banken är nöjd så ändras det till att jämföra summor
+        if ($gamephase == "banksturn") {
+            $session->set("gamephase", "decide");
+        }
+
+        return $this->redirectToRoute('game_play');
+    }
+}
